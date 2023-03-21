5e500d404a85ca00a3178610afb35a881c4b6f8a
Gabriel Decima <gabrieldecima1028@gmail.com>
2023-03-21 18:47:50 -0300

    Seguimos modificando media queris

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 var typed = new Typed(".multiple-text",{
-    strings:["Frontend Developer","Html","Css","React js"],
+    strings:["Frontend"," Developer","Html","Css","React js"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
     loop:true
 })
+
+
+
+
+const hamburguesa = document.getElementById("menu-home");
+const menuHamburguesa = document.getElementById("navegador");
+
+function cambiarEstado(){
+    if(menuHamburguesa.classList.contains('activate-menu')){
+        menuHamburguesa.classList.remove('activate-menu');
+    } else {
+        menuHamburguesa.classList.add('activate-menu');
+    }
+  }
+
+
+hamburguesa.addEventListener("click", cambiarEstado)
